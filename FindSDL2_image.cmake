ea591eda2dbd9411aecae82715a03e805e4923ee
Amine B. Hassouna <amine.benhassouna@gmail.com>
2019-02-03 08:57:45 +0100

    Adapt SDL_image CMake module to SDL2_image and improve it

--- FindSDL2_image.cmake
+++ FindSDL2_image.cmake
@@ -2,52 +2,99 @@
 # file Copyright.txt or https://cmake.org/licensing for details.
 
 #[=======================================================================[.rst:
-FindSDL_image
--------------
+FindSDL2_image
+--------------
 
-Locate SDL_image library
+Locate SDL2_image library
 
-This module defines:
-
-::
-
-  SDL_IMAGE_LIBRARIES, the name of the library to link against
-  SDL_IMAGE_INCLUDE_DIRS, where to find the headers
-  SDL_IMAGE_FOUND, if false, do not try to link against
-  SDL_IMAGE_VERSION_STRING - human-readable string containing the
-                             version of SDL_image
-
-
-
-For backward compatibility the following variables are also set:
+This module will set the following variables in your project:
 
 ::
 
-  SDLIMAGE_LIBRARY (same value as SDL_IMAGE_LIBRARIES)
-  SDLIMAGE_INCLUDE_DIR (same value as SDL_IMAGE_INCLUDE_DIRS)
-  SDLIMAGE_FOUND (same value as SDL_IMAGE_FOUND)
+  SDL2_IMAGE_LIBRARIES, the name of the library to link against
+  SDL2_IMAGE_INCLUDE_DIRS, where to find the headers
+  SDL2_IMAGE_FOUND, if false, do not try to link against
+  SDL2_IMAGE_VERSION_STRING - human-readable string containing the
+                              version of SDL2_image
 
 
 
-$SDLDIR is an environment variable that would correspond to the
-./configure --prefix=$SDLDIR used in building SDL.
+This module responds to the following cache variables:
 
-Created by Eric Wing.  This was influenced by the FindSDL.cmake
-module, but with modifications to recognize OS X frameworks and
-additional Unix paths (FreeBSD, etc).
+::
+
+  SDL2_IMAGE_PATH
+    Set a custom SDL2_image Library path (default: empty)
+
+  SDL2_IMAGE_NO_DEFAULT_PATH
+    Disable search SDL2_image Library in default path.
+      If SDL2_IMAGE_PATH (default: ON)
+      Else (default: OFF)
+
+  SDL2_IMAGE_INCLUDE_DIR
+    SDL2_image headers path.
+
+  SDL2_IMAGE_LIBRARY
+    SDL2_image Library (.dll, .so, .a, etc) path.
+
+
+Additional Note: If you see an empty SDL2_IMAGE_LIBRARY in your project
+configuration, it means CMake did not find your SDL2_image library
+(SDL2_image.dll, libsdl2_image.so, etc). Set SDL2_IMAGE_LIBRARY to point
+to your SDL2_image library, and  configure again. This value is used to
+generate the final SDL2_IMAGE_LIBRARIES variable, but when this value is
+unset, SDL2_IMAGE_LIBRARIES does not get created.
+
+
+$SDL2IMAGEDIR is an environment variable that would correspond to the
+./configure --prefix=$SDL2IMAGEDIR used in building SDL2_image.
+
+$SDL2DIR is an environment variable that would correspond to the
+./configure --prefix=$SDL2DIR used in building SDL2.
+
+
+
+Created by Amine Ben Hassouna:
+  Adapt FindSDL_image.cmake to SDL2_image (FindSDL2_image.cmake).
+  Add cache variables for more flexibility:
+    SDL2_IMAGE_PATH, SDL2_IMAGE_NO_DEFAULT_PATH (for details, see doc above).
+
+Original FindSDL_image.cmake module:
+  Created by Eric Wing.  This was influenced by the FindSDL.cmake
+  module, but with modifications to recognize OS X frameworks and
+  additional Unix paths (FreeBSD, etc).
 #]=======================================================================]
 
-if(NOT SDL_IMAGE_INCLUDE_DIR AND SDLIMAGE_INCLUDE_DIR)
-  set(SDL_IMAGE_INCLUDE_DIR ${SDLIMAGE_INCLUDE_DIR} CACHE PATH "directory cache
-entry initialized from old variable name")
+# Define options for searching SDL2_image Library in a custom path
+
+set(SDL2_IMAGE_PATH "" CACHE STRING "Custom SDL2_image Library path")
+
+set(_SDL2_IMAGE_NO_DEFAULT_PATH OFF)
+if(SDL2_IMAGE_PATH)
+  set(_SDL2_IMAGE_NO_DEFAULT_PATH ON)
 endif()
-find_path(SDL_IMAGE_INCLUDE_DIR SDL_image.h
+
+set(SDL2_IMAGE_NO_DEFAULT_PATH ${_SDL2_IMAGE_NO_DEFAULT_PATH}
+    CACHE BOOL "Disable search SDL2_image Library in default path")
+unset(_SDL2_IMAGE_NO_DEFAULT_PATH)
+
+set(SDL2_IMAGE_NO_DEFAULT_PATH_CMD)
+if(SDL2_IMAGE_NO_DEFAULT_PATH)
+  set(SDL2_IMAGE_NO_DEFAULT_PATH_CMD NO_DEFAULT_PATH)
+endif()
+
+# Search for the SDL2_image include directory
+find_path(SDL2_IMAGE_INCLUDE_DIR SDL_image.h
   HINTS
-    ENV SDLIMAGEDIR
-    ENV SDLDIR
-  PATH_SUFFIXES SDL
-                # path suffixes to search inside ENV{SDLDIR}
-                include/SDL include/SDL12 include/SDL11 include
+    ENV SDL2IMAGEDIR
+    ENV SDL2DIR
+    ${SDL2_IMAGE_NO_DEFAULT_PATH_CMD}
+  PATH_SUFFIXES SDL2
+                # path suffixes to search inside ENV{SDL2DIR}
+                # and ENV{SDL2IMAGEDIR}
+                include/SDL2 include
+  PATHS ${SDL2_IMAGE_PATH}
+  DOC "Where the SDL2_image headers can be found"
 )
 
 if(CMAKE_SIZEOF_VOID_P EQUAL 8)
@@ -56,46 +103,47 @@ else()
   set(VC_LIB_PATH_SUFFIX lib/x86)
 endif()
 
-if(NOT SDL_IMAGE_LIBRARY AND SDLIMAGE_LIBRARY)
-  set(SDL_IMAGE_LIBRARY ${SDLIMAGE_LIBRARY} CACHE FILEPATH "file cache entry
-initialized from old variable name")
-endif()
-find_library(SDL_IMAGE_LIBRARY
-  NAMES SDL_image
+# Search for the SDL2_image library
+find_library(SDL2_IMAGE_LIBRARY
+  NAMES SDL2_image
   HINTS
-    ENV SDLIMAGEDIR
-    ENV SDLDIR
+    ENV SDL2IMAGEDIR
+    ENV SDL2DIR
+    ${SDL2_IMAGE_NO_DEFAULT_PATH_CMD}
   PATH_SUFFIXES lib ${VC_LIB_PATH_SUFFIX}
+  PATHS ${SDL2_IMAGE_PATH}
+  DOC "Where the SDL2_image Library can be found"
 )
 
-if(SDL_IMAGE_INCLUDE_DIR AND EXISTS "${SDL_IMAGE_INCLUDE_DIR}/SDL_image.h")
-  file(STRINGS "${SDL_IMAGE_INCLUDE_DIR}/SDL_image.h" SDL_IMAGE_VERSION_MAJOR_LINE REGEX "^#define[ \t]+SDL_IMAGE_MAJOR_VERSION[ \t]+[0-9]+$")
-  file(STRINGS "${SDL_IMAGE_INCLUDE_DIR}/SDL_image.h" SDL_IMAGE_VERSION_MINOR_LINE REGEX "^#define[ \t]+SDL_IMAGE_MINOR_VERSION[ \t]+[0-9]+$")
-  file(STRINGS "${SDL_IMAGE_INCLUDE_DIR}/SDL_image.h" SDL_IMAGE_VERSION_PATCH_LINE REGEX "^#define[ \t]+SDL_IMAGE_PATCHLEVEL[ \t]+[0-9]+$")
-  string(REGEX REPLACE "^#define[ \t]+SDL_IMAGE_MAJOR_VERSION[ \t]+([0-9]+)$" "\\1" SDL_IMAGE_VERSION_MAJOR "${SDL_IMAGE_VERSION_MAJOR_LINE}")
-  string(REGEX REPLACE "^#define[ \t]+SDL_IMAGE_MINOR_VERSION[ \t]+([0-9]+)$" "\\1" SDL_IMAGE_VERSION_MINOR "${SDL_IMAGE_VERSION_MINOR_LINE}")
-  string(REGEX REPLACE "^#define[ \t]+SDL_IMAGE_PATCHLEVEL[ \t]+([0-9]+)$" "\\1" SDL_IMAGE_VERSION_PATCH "${SDL_IMAGE_VERSION_PATCH_LINE}")
-  set(SDL_IMAGE_VERSION_STRING ${SDL_IMAGE_VERSION_MAJOR}.${SDL_IMAGE_VERSION_MINOR}.${SDL_IMAGE_VERSION_PATCH})
-  unset(SDL_IMAGE_VERSION_MAJOR_LINE)
-  unset(SDL_IMAGE_VERSION_MINOR_LINE)
-  unset(SDL_IMAGE_VERSION_PATCH_LINE)
-  unset(SDL_IMAGE_VERSION_MAJOR)
-  unset(SDL_IMAGE_VERSION_MINOR)
-  unset(SDL_IMAGE_VERSION_PATCH)
+# Read SDL2_image version
+if(SDL2_IMAGE_INCLUDE_DIR AND EXISTS "${SDL2_IMAGE_INCLUDE_DIR}/SDL_image.h")
+  file(STRINGS "${SDL2_IMAGE_INCLUDE_DIR}/SDL_image.h" SDL2_IMAGE_VERSION_MAJOR_LINE REGEX "^#define[ \t]+SDL_IMAGE_MAJOR_VERSION[ \t]+[0-9]+$")
+  file(STRINGS "${SDL2_IMAGE_INCLUDE_DIR}/SDL_image.h" SDL2_IMAGE_VERSION_MINOR_LINE REGEX "^#define[ \t]+SDL_IMAGE_MINOR_VERSION[ \t]+[0-9]+$")
+  file(STRINGS "${SDL2_IMAGE_INCLUDE_DIR}/SDL_image.h" SDL2_IMAGE_VERSION_PATCH_LINE REGEX "^#define[ \t]+SDL_IMAGE_PATCHLEVEL[ \t]+[0-9]+$")
+  string(REGEX REPLACE "^#define[ \t]+SDL_IMAGE_MAJOR_VERSION[ \t]+([0-9]+)$" "\\1" SDL2_IMAGE_VERSION_MAJOR "${SDL2_IMAGE_VERSION_MAJOR_LINE}")
+  string(REGEX REPLACE "^#define[ \t]+SDL_IMAGE_MINOR_VERSION[ \t]+([0-9]+)$" "\\1" SDL2_IMAGE_VERSION_MINOR "${SDL2_IMAGE_VERSION_MINOR_LINE}")
+  string(REGEX REPLACE "^#define[ \t]+SDL_IMAGE_PATCHLEVEL[ \t]+([0-9]+)$" "\\1" SDL2_IMAGE_VERSION_PATCH "${SDL2_IMAGE_VERSION_PATCH_LINE}")
+  set(SDL2_IMAGE_VERSION_STRING ${SDL2_IMAGE_VERSION_MAJOR}.${SDL2_IMAGE_VERSION_MINOR}.${SDL2_IMAGE_VERSION_PATCH})
+  unset(SDL2_IMAGE_VERSION_MAJOR_LINE)
+  unset(SDL2_IMAGE_VERSION_MINOR_LINE)
+  unset(SDL2_IMAGE_VERSION_PATCH_LINE)
+  unset(SDL2_IMAGE_VERSION_MAJOR)
+  unset(SDL2_IMAGE_VERSION_MINOR)
+  unset(SDL2_IMAGE_VERSION_PATCH)
 endif()
 
-set(SDL_IMAGE_LIBRARIES ${SDL_IMAGE_LIBRARY})
-set(SDL_IMAGE_INCLUDE_DIRS ${SDL_IMAGE_INCLUDE_DIR})
+set(SDL2_IMAGE_LIBRARIES ${SDL2_IMAGE_LIBRARY})
+set(SDL2_IMAGE_INCLUDE_DIRS ${SDL2_IMAGE_INCLUDE_DIR})
 
 include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(SDL_image
-                                  REQUIRED_VARS SDL_IMAGE_LIBRARIES SDL_IMAGE_INCLUDE_DIRS
-                                  VERSION_VAR SDL_IMAGE_VERSION_STRING)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SDL2_image
+                                  REQUIRED_VARS SDL2_IMAGE_LIBRARIES SDL2_IMAGE_INCLUDE_DIRS
+                                  VERSION_VAR SDL2_IMAGE_VERSION_STRING)
 
-# for backward compatibility
-set(SDLIMAGE_LIBRARY ${SDL_IMAGE_LIBRARIES})
-set(SDLIMAGE_INCLUDE_DIR ${SDL_IMAGE_INCLUDE_DIRS})
-set(SDLIMAGE_FOUND ${SDL_IMAGE_FOUND})
 
-mark_as_advanced(SDL_IMAGE_LIBRARY SDL_IMAGE_INCLUDE_DIR)
+mark_as_advanced(SDL2_IMAGE_PATH
+                 SDL2_IMAGE_NO_DEFAULT_PATH
+                 SDL2_IMAGE_LIBRARY
+                 SDL2_IMAGE_INCLUDE_DIR)
+
