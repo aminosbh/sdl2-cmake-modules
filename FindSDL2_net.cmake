190d4492a45906208b4e792c7a4c17651c57d25c
Amine B. Hassouna <amine.benhassouna@gmail.com>
2019-02-03 09:08:00 +0100

    Adapt SDL_net CMake module to SDL2_net and improve it

--- FindSDL2_net.cmake
+++ FindSDL2_net.cmake
@@ -2,51 +2,99 @@
 # file Copyright.txt or https://cmake.org/licensing for details.
 
 #[=======================================================================[.rst:
-FindSDL_net
------------
+FindSDL2_net
+------------
 
-Locate SDL_net library
+Locate SDL2_net library
 
-This module defines:
-
-::
-
-  SDL_NET_LIBRARIES, the name of the library to link against
-  SDL_NET_INCLUDE_DIRS, where to find the headers
-  SDL_NET_FOUND, if false, do not try to link against
-  SDL_NET_VERSION_STRING - human-readable string containing the version of SDL_net
-
-
-
-For backward compatibility the following variables are also set:
+This module will set the following variables in your project:
 
 ::
 
-  SDLNET_LIBRARY (same value as SDL_NET_LIBRARIES)
-  SDLNET_INCLUDE_DIR (same value as SDL_NET_INCLUDE_DIRS)
-  SDLNET_FOUND (same value as SDL_NET_FOUND)
+  SDL2_NET_LIBRARIES, the name of the library to link against
+  SDL2_NET_INCLUDE_DIRS, where to find the headers
+  SDL2_NET_FOUND, if false, do not try to link against
+  SDL2_NET_VERSION_STRING - human-readable string containing the
+                            version of SDL2_net
 
 
 
-$SDLDIR is an environment variable that would correspond to the
-./configure --prefix=$SDLDIR used in building SDL.
+This module responds to the following cache variables:
 
-Created by Eric Wing.  This was influenced by the FindSDL.cmake
-module, but with modifications to recognize OS X frameworks and
-additional Unix paths (FreeBSD, etc).
+::
+
+  SDL2_NET_PATH
+    Set a custom SDL2_net Library path (default: empty)
+
+  SDL2_NET_NO_DEFAULT_PATH
+    Disable search SDL2_net Library in default path.
+      If SDL2_NET_PATH (default: ON)
+      Else (default: OFF)
+
+  SDL2_NET_INCLUDE_DIR
+    SDL2_net headers path.
+
+  SDL2_NET_LIBRARY
+    SDL2_net Library (.dll, .so, .a, etc) path.
+
+
+Additional Note: If you see an empty SDL2_NET_LIBRARY in your project
+configuration, it means CMake did not find your SDL2_net library
+(SDL2_net.dll, libsdl2_net.so, etc). Set SDL2_NET_LIBRARY to point
+to your SDL2_net library, and  configure again. This value is used to
+generate the final SDL2_NET_LIBRARIES variable, but when this value is
+unset, SDL2_NET_LIBRARIES does not get created.
+
+
+$SDL2NETDIR is an environment variable that would correspond to the
+./configure --prefix=$SDL2NETDIR used in building SDL2_net.
+
+$SDL2DIR is an environment variable that would correspond to the
+./configure --prefix=$SDL2DIR used in building SDL2.
+
+
+
+Created by Amine Ben Hassouna:
+  Adapt FindSDL_net.cmake to SDL2_net (FindSDL2_net.cmake).
+  Add cache variables for more flexibility:
+    SDL2_NET_PATH, SDL2_NET_NO_DEFAULT_PATH (for details, see doc above).
+
+Original FindSDL_net.cmake module:
+  Created by Eric Wing.  This was influenced by the FindSDL.cmake
+  module, but with modifications to recognize OS X frameworks and
+  additional Unix paths (FreeBSD, etc).
 #]=======================================================================]
 
-if(NOT SDL_NET_INCLUDE_DIR AND SDLNET_INCLUDE_DIR)
-  set(SDL_NET_INCLUDE_DIR ${SDLNET_INCLUDE_DIR} CACHE PATH "directory cache
-entry initialized from old variable name")
+# Define options for searching SDL2_net Library in a custom path
+
+set(SDL2_NET_PATH "" CACHE STRING "Custom SDL2_net Library path")
+
+set(_SDL2_NET_NO_DEFAULT_PATH OFF)
+if(SDL2_NET_PATH)
+  set(_SDL2_NET_NO_DEFAULT_PATH ON)
 endif()
-find_path(SDL_NET_INCLUDE_DIR SDL_net.h
+
+set(SDL2_NET_NO_DEFAULT_PATH ${_SDL2_NET_NO_DEFAULT_PATH}
+    CACHE BOOL "Disable search SDL2_net Library in default path")
+unset(_SDL2_NET_NO_DEFAULT_PATH)
+
+set(SDL2_NET_NO_DEFAULT_PATH_CMD)
+if(SDL2_NET_NO_DEFAULT_PATH)
+  set(SDL2_NET_NO_DEFAULT_PATH_CMD NO_DEFAULT_PATH)
+endif()
+
+# Search for the SDL2_net include directory
+find_path(SDL2_NET_INCLUDE_DIR SDL_net.h
   HINTS
-    ENV SDLNETDIR
-    ENV SDLDIR
-  PATH_SUFFIXES SDL
-                # path suffixes to search inside ENV{SDLDIR}
-                include/SDL include/SDL12 include/SDL11 include
+    ENV SDL2NETDIR
+    ENV SDL2DIR
+    ${SDL2_NET_NO_DEFAULT_PATH_CMD}
+  PATH_SUFFIXES SDL2
+                # path suffixes to search inside ENV{SDL2DIR}
+                # and ENV{SDL2NETDIR}
+                include/SDL2 include
+  PATHS ${SDL2_NET_PATH}
+  DOC "Where the SDL2_net headers can be found"
 )
 
 if(CMAKE_SIZEOF_VOID_P EQUAL 8)
@@ -55,46 +103,47 @@ else()
   set(VC_LIB_PATH_SUFFIX lib/x86)
 endif()
 
-if(NOT SDL_NET_LIBRARY AND SDLNET_LIBRARY)
-  set(SDL_NET_LIBRARY ${SDLNET_LIBRARY} CACHE FILEPATH "file cache entry
-initialized from old variable name")
-endif()
-find_library(SDL_NET_LIBRARY
-  NAMES SDL_net
+# Search for the SDL2_net library
+find_library(SDL2_NET_LIBRARY
+  NAMES SDL2_net
   HINTS
-    ENV SDLNETDIR
-    ENV SDLDIR
+    ENV SDL2NETDIR
+    ENV SDL2DIR
+    ${SDL2_NET_NO_DEFAULT_PATH_CMD}
   PATH_SUFFIXES lib ${VC_LIB_PATH_SUFFIX}
+  PATHS ${SDL2_NET_PATH}
+  DOC "Where the SDL2_net Library can be found"
 )
 
-if(SDL_NET_INCLUDE_DIR AND EXISTS "${SDL_NET_INCLUDE_DIR}/SDL_net.h")
-  file(STRINGS "${SDL_NET_INCLUDE_DIR}/SDL_net.h" SDL_NET_VERSION_MAJOR_LINE REGEX "^#define[ \t]+SDL_NET_MAJOR_VERSION[ \t]+[0-9]+$")
-  file(STRINGS "${SDL_NET_INCLUDE_DIR}/SDL_net.h" SDL_NET_VERSION_MINOR_LINE REGEX "^#define[ \t]+SDL_NET_MINOR_VERSION[ \t]+[0-9]+$")
-  file(STRINGS "${SDL_NET_INCLUDE_DIR}/SDL_net.h" SDL_NET_VERSION_PATCH_LINE REGEX "^#define[ \t]+SDL_NET_PATCHLEVEL[ \t]+[0-9]+$")
-  string(REGEX REPLACE "^#define[ \t]+SDL_NET_MAJOR_VERSION[ \t]+([0-9]+)$" "\\1" SDL_NET_VERSION_MAJOR "${SDL_NET_VERSION_MAJOR_LINE}")
-  string(REGEX REPLACE "^#define[ \t]+SDL_NET_MINOR_VERSION[ \t]+([0-9]+)$" "\\1" SDL_NET_VERSION_MINOR "${SDL_NET_VERSION_MINOR_LINE}")
-  string(REGEX REPLACE "^#define[ \t]+SDL_NET_PATCHLEVEL[ \t]+([0-9]+)$" "\\1" SDL_NET_VERSION_PATCH "${SDL_NET_VERSION_PATCH_LINE}")
-  set(SDL_NET_VERSION_STRING ${SDL_NET_VERSION_MAJOR}.${SDL_NET_VERSION_MINOR}.${SDL_NET_VERSION_PATCH})
-  unset(SDL_NET_VERSION_MAJOR_LINE)
-  unset(SDL_NET_VERSION_MINOR_LINE)
-  unset(SDL_NET_VERSION_PATCH_LINE)
-  unset(SDL_NET_VERSION_MAJOR)
-  unset(SDL_NET_VERSION_MINOR)
-  unset(SDL_NET_VERSION_PATCH)
+# Read SDL2_net version
+if(SDL2_NET_INCLUDE_DIR AND EXISTS "${SDL2_NET_INCLUDE_DIR}/SDL_net.h")
+  file(STRINGS "${SDL2_NET_INCLUDE_DIR}/SDL_net.h" SDL2_NET_VERSION_MAJOR_LINE REGEX "^#define[ \t]+SDL_NET_MAJOR_VERSION[ \t]+[0-9]+$")
+  file(STRINGS "${SDL2_NET_INCLUDE_DIR}/SDL_net.h" SDL2_NET_VERSION_MINOR_LINE REGEX "^#define[ \t]+SDL_NET_MINOR_VERSION[ \t]+[0-9]+$")
+  file(STRINGS "${SDL2_NET_INCLUDE_DIR}/SDL_net.h" SDL2_NET_VERSION_PATCH_LINE REGEX "^#define[ \t]+SDL_NET_PATCHLEVEL[ \t]+[0-9]+$")
+  string(REGEX REPLACE "^#define[ \t]+SDL_NET_MAJOR_VERSION[ \t]+([0-9]+)$" "\\1" SDL2_NET_VERSION_MAJOR "${SDL2_NET_VERSION_MAJOR_LINE}")
+  string(REGEX REPLACE "^#define[ \t]+SDL_NET_MINOR_VERSION[ \t]+([0-9]+)$" "\\1" SDL2_NET_VERSION_MINOR "${SDL2_NET_VERSION_MINOR_LINE}")
+  string(REGEX REPLACE "^#define[ \t]+SDL_NET_PATCHLEVEL[ \t]+([0-9]+)$" "\\1" SDL2_NET_VERSION_PATCH "${SDL2_NET_VERSION_PATCH_LINE}")
+  set(SDL2_NET_VERSION_STRING ${SDL2_NET_VERSION_MAJOR}.${SDL2_NET_VERSION_MINOR}.${SDL2_NET_VERSION_PATCH})
+  unset(SDL2_NET_VERSION_MAJOR_LINE)
+  unset(SDL2_NET_VERSION_MINOR_LINE)
+  unset(SDL2_NET_VERSION_PATCH_LINE)
+  unset(SDL2_NET_VERSION_MAJOR)
+  unset(SDL2_NET_VERSION_MINOR)
+  unset(SDL2_NET_VERSION_PATCH)
 endif()
 
-set(SDL_NET_LIBRARIES ${SDL_NET_LIBRARY})
-set(SDL_NET_INCLUDE_DIRS ${SDL_NET_INCLUDE_DIR})
+set(SDL2_NET_LIBRARIES ${SDL2_NET_LIBRARY})
+set(SDL2_NET_INCLUDE_DIRS ${SDL2_NET_INCLUDE_DIR})
 
 include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(SDL_net
-                                  REQUIRED_VARS SDL_NET_LIBRARIES SDL_NET_INCLUDE_DIRS
-                                  VERSION_VAR SDL_NET_VERSION_STRING)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SDL2_net
+                                  REQUIRED_VARS SDL2_NET_LIBRARIES SDL2_NET_INCLUDE_DIRS
+                                  VERSION_VAR SDL2_NET_VERSION_STRING)
 
-# for backward compatibility
-set(SDLNET_LIBRARY ${SDL_NET_LIBRARIES})
-set(SDLNET_INCLUDE_DIR ${SDL_NET_INCLUDE_DIRS})
-set(SDLNET_FOUND ${SDL_NET_FOUND})
 
-mark_as_advanced(SDL_NET_LIBRARY SDL_NET_INCLUDE_DIR)
+mark_as_advanced(SDL2_NET_PATH
+                 SDL2_NET_NO_DEFAULT_PATH
+                 SDL2_NET_LIBRARY
+                 SDL2_NET_INCLUDE_DIR)
+
