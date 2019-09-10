df94e78609e74d5f6766cf59ba691f0b375a22f6
Ryan Pavlik <rpavlik@gmail.com>
2019-09-10 15:50:28 -0500

    Fix threads check, since CMAKE_THREAD_LIBS_INIT may be validly-blank

--- FindSDL2.cmake
+++ FindSDL2.cmake
@@ -236,8 +236,8 @@ endif()
 # frameworks may already provide it.
 # But for non-OSX systems, I will use the CMake Threads package.
 if(NOT APPLE)
-  find_package(Threads QUIET)
-  if(NOT CMAKE_THREAD_LIBS_INIT)
+  find_package(Threads)
+  if(NOT Threads_FOUND)
     set(SDL2_THREADS_NOT_FOUND "Could NOT find Threads (Threads is required by SDL2).")
     if(SDL2_FIND_REQUIRED)
       message(FATAL_ERROR ${SDL2_THREADS_NOT_FOUND})
