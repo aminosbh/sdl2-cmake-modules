92d390139bf6c0dfea7bcf1c7d6c981fa8ab6800
tseiler <todd.seiler@gmail.com>
2020-07-25 18:05:28 -0400

    Fix for name mismatched cmake warning.

--- FindSDL2.cmake
+++ FindSDL2.cmake
@@ -317,7 +317,8 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(SDL2
 if(SDL2MAIN_LIBRARY)
   FIND_PACKAGE_HANDLE_STANDARD_ARGS(SDL2main
                                     REQUIRED_VARS SDL2MAIN_LIBRARY SDL2_INCLUDE_DIR
-                                    VERSION_VAR SDL2_VERSION_STRING)
+                                    VERSION_VAR SDL2_VERSION_STRING
+                                    NAME_MISMATCHED)
 endif()
 
 
